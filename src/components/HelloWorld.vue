b3827ab2f4fcd8912f7b0c76f7470a5a49a4363f
nicolas blum <nblumferracci@gmail.com>
2021-02-15 16:03:53 +0100

    prefetch the component with a conditional rendering

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,19 +3,28 @@
     <div class="hello">
       <h1>{{ msg }}</h1>
     </div>
-    <BigYellowUsername username="Nico" />
+    <button @click="showUsername = !showUsername">Toggle Username</button>
+    <div v-if="showUsername">
+      <BigYellowUsername username="Nico" />
+    </div>
   </div>
 </template>
 
 <script>
-import BigYellowUsername from './BigYellowUsername.vue'
+// import BigYellowUsername from './BigYellowUsername.vue'
 export default {
+  data () {
+    return {
+      showUsername: false
+    }
+  },
   name: 'HelloWorld',
   props: {
     msg: String
   },
   components: {
-    BigYellowUsername
+    BigYellowUsername: () => import(/* webpackChunkName: "BigYellowUsername" */'./BigYellowUsername')
+    // because we conditionaly render the username, we can prefetch the component and load when needed
   }
 }
 </script>
